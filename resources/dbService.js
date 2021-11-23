1959c29812bf531536344d43d7f12fca4327ed3b
ThomasAK <thomas.a.k.tk@gmail.com>
2021-11-22 18:08:34 -0700

    changed how i was saving user activity to the DB so i could simplify db calls and simplify code on activityrole

--- resources/dbService.js
+++ resources/dbService.js
@@ -25,34 +25,19 @@ class dbService {
         return results[0]
     }
 
-
-    async getUser(userID) {
-        const query = "SELECT * FROM users WHERE userID = '" + userID + "'";
-        const user = await this.runQuery(query)
-        return user[0];
-    }
-
-
-    async addUser(userID) {
-        const query = "INSERT INTO users (userID, lastActivity, first, second, third, forth, fifth, sixth, week1, week2, week3, week4, week5, week6)"
-            + " VALUES ('" + userID + "','" + today + "', '1' , '0', '0', '0', '0', '0','" + today + "','" + today + "','" + today + "','" + today + "','" + today + "','" + today + "')"
+    async addUserActivity(userID){
+        const query = "INSERT INTO user_activity VALUES (" + userID + ", '" + today+ "')"
         await this.runQuery(query)
     }
 
-    async updateUser(user) {
-        const query = "UPDATE users SET "
-            +"first = '" + user.first + "', second = '" + user.second + "', third = '" + user.third + "', forth = '" + user.forth + "', fifth = '" + user.fifth + "', sixth = '" + user.sixth
-            +"', week1 = '" + user.week1 + "', week2 = '" + user.week2 + "', week3 = '" + user.week3 + "', week4 = '" + user.week4 + "', week5 = '" + user.week5 + "', week6 = '" + user.week6
-            + "', lastActivity = '" + user.lastActivity + "' WHERE userID = '" + user.userID + "'"
-        await this.runQuery(query)
+    async getActivityCount(userID, date){
+        const query ="SELECT count(*) AS activityCount FROM user_activity WHERE user_id = '"+ userID + "' AND activity_date >= '" + date + "'"
+        return await this.runQuery(query)
     }
 
-    async deleteUser(userID) {
-        const query = "DELETE FROM users WHERE userID = '" + userID + "'"
-        await this.runQuery(query)
-    }
 
 
 }
+
 
 module.exports = dbService
