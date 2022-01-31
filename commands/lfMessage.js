c952a683af227e80756f904f0d87913cc1a0db6b
ThomasAK <thomas.a.k.tk@gmail.com>
2022-01-31 15:45:49 -0700

    Updated lfmessage to include user's name

--- commands/lfMessage.js
+++ commands/lfMessage.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'This will create a LFG message that includes a join button for the channel the user is in.',
     async execute(message){
         if (!message.member.voice.channel) return message.channel.send("You must be in a voice channel for this to work.");
-        message.channel.send(`${message.content.slice(4)} \n ${await message.member.voice.channel.createInvite()}`).then()
+        message.channel.send(`@${message.member.nickname} ${message.content.slice(4)} \n ${await message.member.voice.channel.createInvite()}`).then()
             .catch(console.error);
         message.delete()
     }
