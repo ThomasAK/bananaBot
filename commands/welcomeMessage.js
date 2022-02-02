c576e4139b1da22a8ef8be9e7732fd7a5431fbf4
ThomasAK <thomas.a.k.tk@gmail.com>
2022-02-02 09:37:36 -0700

    Added welcomeMessage.js to the discord bot.

--- commands/welcomeMessage.js
+++ commands/welcomeMessage.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Adds welcome message to welcome channel when a new user joins the guild.',
     async execute(member, client){
         const welcomeChannel = await client.channels.fetch(data.welcomeChannel)
-        await welcomeChannel.send(`Hello ${member.displayName}, Welcome to ${member.guild.name} to get started please check out the rules channel under server info, then to be able to join voice channels go to role-selection 
+        await welcomeChannel.send(`Hello ${member}, Welcome to ${member.guild.name} to get started please check out the rules channel under server info, then to be able to join voice channels go to role-selection 
         under First Stop and select the roles you would like. We appreciate you choosing us and hope you have an amazing gaming experience with us. If you have any questions or concerns please message a Commander and Chief or a Mod Boss. `)
     }
 }
