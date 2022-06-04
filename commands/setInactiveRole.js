b179028d13572fd3365ff1863761874bb058e5a9
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-04 00:15:36 -0600

    fix logical error.

--- commands/setInactiveRole.js
+++ commands/setInactiveRole.js
@@ -6,10 +6,10 @@ module.exports = {
         const db = new dbService()
         const past90Days = new Date(Date.now() - 90 * 24 * 60 * 60 * 1000)
         message.channel.send('finding inactive users')
-        function getRole (roleName){return member.roles.cache.find(role => role.name === roleName);}
+        function getRole (roleName, member){return member.roles.cache.find(role => role.name === roleName);}
 
         for (const member of message.guild.members.cache ){
-            if (getRole('Commander in Chief') || getRole('Mod Boss') || getRole('OG') || getRole('Server Booster') || getRole('Regular')) return
+            if (getRole('Commander in Chief', member) || getRole('Mod Boss', member) || getRole('OG', member) || getRole('Server Booster', member) || getRole('Regular', member)) return
             console.log(member.displayName)
             const count = await db.getActivityCount(member.id, past90Days.getFullYear() + '-' + (past90Days.getMonth()+1) + '-' + past90Days.getDate())
             console.log(count)
