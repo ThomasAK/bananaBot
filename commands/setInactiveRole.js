42ee5ae35638c99c54c3a157bcf985c71095a236
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-04 00:20:46 -0600

    Bug fix

--- commands/setInactiveRole.js
+++ commands/setInactiveRole.js
@@ -8,7 +8,7 @@ module.exports = {
         message.channel.send('finding inactive users')
         function getRole (roleName, member){return member.roles.cache.find(role => role.name === roleName);}
 
-        for (const member of message.guild.members.cache ){
+        for (const member of message.guild.members.fetch() ){
             if (getRole('Commander in Chief', member) || getRole('Mod Boss', member) || getRole('OG', member) || getRole('Server Booster', member) || getRole('Regular', member)) return
             console.log(member.displayName)
             const count = await db.getActivityCount(member.id, past90Days.getFullYear() + '-' + (past90Days.getMonth()+1) + '-' + past90Days.getDate())
