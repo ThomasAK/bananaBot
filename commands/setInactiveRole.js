656f0482d9107842f243260fc8c3fb99efa1d6cf
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-03 23:57:37 -0600

    add logs

--- commands/setInactiveRole.js
+++ commands/setInactiveRole.js
@@ -5,17 +5,18 @@ class SetInactiveRole {
         const db = new dbService()
         const past90Days = new Date(Date.now() - 90 * 24 * 60 * 60 * 1000)
 
+        console.log(message.guild.members.cache)
         for (const member of message.guild.members.cache ){
             function getRole (roleName){return member.roles.cache.find(role => role.name === roleName);}
             if (getRole('Commander in Chief') || getRole('Mod Boss') || getRole('OG') || getRole('Server Booster') || getRole('Regular')) return
-
+            console.log(member.displayName)
             const count = await db.getActivityCount(member.id, past90Days.getFullYear() + '-' + (past90Days.getMonth()+1) + '-' + past90Days.getDate())
+            console.log(count)
             const inactiveRole = message.guild.roles.cache.find(role => role.name === 'Inactive')
             if (count < 1) {
                 //await member.roles.remove(member.roles.cache);
                 await member.roles.add(inactiveRole);
             }
-
         }
     }
 }
