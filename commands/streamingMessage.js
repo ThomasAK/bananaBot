909a9f18d7fb14a81a91d8d4e96ded4108ec62a8
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-03 18:50:54 -0600

    Added check to verify user is streaming in the guild. That way we aren't posting streams that are happening in other guilds.

--- commands/streamingMessage.js
+++ commands/streamingMessage.js
@@ -3,6 +3,7 @@ const data = require("../data.json")
 module.exports = {
     name: 'streamingMessage',
     async execute(presence, activity){
+        if (!presence.member.voice.guild.equals(presence.guild)) return
        const channel = await presence.guild.channels.cache.get(data.streamingChannel)
         channel.send(`@here \n ${presence.member.displayName} is STREAMING ${activity.details} NOW! \n ${activity.url}`)
     }
