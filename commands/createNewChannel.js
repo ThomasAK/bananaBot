d27a8a91201ea3a945928f6273ae5ab1efca2b5b
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-17 21:15:06 -0600

    Added AFK to remove channel check.  Added a while loop to add a extra number to channel name if it is already created.

--- commands/createNewChannel.js
+++ commands/createNewChannel.js
@@ -5,6 +5,9 @@ module.exports = {
     description: 'This creates a new channel',
     async execute(message){
         const messageDetails = message.content.split(/ +/)
+        while (message.guild.channels.cache.find(channel => channel.name === messageDetails[1])) {
+            messageDetails[1] += 1
+        }
         await message.guild.channels.create(`${messageDetails[1]}`, {
             type: 'GUILD_VOICE',
             userLimit: messageDetails[2] ? messageDetails[2] : 99
