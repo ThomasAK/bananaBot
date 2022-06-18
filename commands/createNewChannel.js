6157741a4ace559ff7a6de5c2397b627dd57cdaf
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-17 20:43:05 -0600

    Fixed new channel position

--- commands/createNewChannel.js
+++ commands/createNewChannel.js
@@ -3,14 +3,14 @@ const data = require('../data.json')
 module.exports = {
     name: 'createChannel',
     description: 'This creates a new channel',
-    execute(message){
+    async execute(message){
         const messageDetails = message.content.split(/ +/)
-        message.guild.channels.create(`${messageDetails[1]}`, {
+        await message.guild.channels.create(`${messageDetails[1]}`, {
             type: 'GUILD_VOICE',
             userLimit: messageDetails[2] ? messageDetails[2] : 99
         })
 
-        const newChannel = message.guild.channels.cache.find(channel => channel.name === messageDetails[1])
-        newChannel.setParent(data.guilds[1].gameCategory).then()
+        const newChannel =await message.guild.channels.cache.find(channel => channel.name === messageDetails[1])
+        await newChannel.setParent(data.guilds[1].gameCategory)
     }
 }
