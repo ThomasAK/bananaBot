f0d4aefe018372412307d880e32b0c58a78042d1
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-17 20:13:00 -0600

    fix if or statment

--- commands/removeChannel.js
+++ commands/removeChannel.js
@@ -1,7 +1,7 @@
 
 class RemoveChannel {
     execute(oldState){
-        if (oldState.channel.name === 'Hang Out' || 'Admin Voice') return
+        if (oldState.channel.name === 'Hang Out' || oldState.channel.name ===  'Admin Voice') return
         if (oldState.channel.members.size < 1) oldState.channel.delete('Channel Empty')
     }
 }
