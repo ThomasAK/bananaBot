eef8a06761d9923662a18cac60e90e3a7f95c7f8
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-17 20:11:30 -0600

    Add check for Hang out and Admin channel.

--- commands/removeChannel.js
+++ commands/removeChannel.js
@@ -1,6 +1,7 @@
 
 class RemoveChannel {
     execute(oldState){
+        if (oldState.channel.name === 'Hang Out' || 'Admin Voice') return
         if (oldState.channel.members.size < 1) oldState.channel.delete('Channel Empty')
     }
 }
