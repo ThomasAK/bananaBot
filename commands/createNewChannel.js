45d095fa69b2b760158f2f447064add59402ba4b
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-17 22:02:13 -0600

    fix duplicate channel name.

--- commands/createNewChannel.js
+++ commands/createNewChannel.js
@@ -5,10 +5,12 @@ module.exports = {
     description: 'This creates a new channel',
     async execute(message){
         const messageDetails = message.content.split(/ +/)
-        while (message.guild.channels.cache.find(channel => channel.name === messageDetails[1])) {
-            messageDetails[1] += 1
+        let num
+        while (message.guild.channels.cache.find(channel => channel.name === messageDetails[1]+num)) {
+            num++
         }
-        await message.guild.channels.create(`${messageDetails[1]}`, {
+        const name = num < 1 ? messageDetails[1] : `${messageDetails[1]}${num}`
+        await message.guild.channels.create(name , {
             type: 'GUILD_VOICE',
             userLimit: messageDetails[2] ? messageDetails[2] : 99
         })
