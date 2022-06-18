8174cf40791cb0644d860b1720de705fd0867662
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-17 20:41:39 -0600

    Fixed new channel position

--- commands/createNewChannel.js
+++ commands/createNewChannel.js
@@ -5,10 +5,12 @@ module.exports = {
     description: 'This creates a new channel',
     execute(message){
         const messageDetails = message.content.split(/ +/)
-        const newChannel = message.guild.channels.create(`${messageDetails[1]}`, {
+        message.guild.channels.create(`${messageDetails[1]}`, {
             type: 'GUILD_VOICE',
             userLimit: messageDetails[2] ? messageDetails[2] : 99
         })
+
+        const newChannel = message.guild.channels.cache.find(channel => channel.name === messageDetails[1])
         newChannel.setParent(data.guilds[1].gameCategory).then()
     }
 }
