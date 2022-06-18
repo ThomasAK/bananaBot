6fab87d47733782269df496937b5f0fb2343da68
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-17 20:07:21 -0600

    remove logs

--- commands/removeChannel.js
+++ commands/removeChannel.js
@@ -1,7 +1,6 @@
 
 class RemoveChannel {
     execute(oldState){
-        console.log(oldState.channel.size)
         if (oldState.channel.members.size < 1) oldState.channel.delete('Channel Empty')
     }
 }
