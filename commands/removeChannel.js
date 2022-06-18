8feba9d299357bfeaaa907417165a10bd6c4b2cb
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-17 20:14:20 -0600

    add waiting for squad in the check.

--- commands/removeChannel.js
+++ commands/removeChannel.js
@@ -1,7 +1,7 @@
 
 class RemoveChannel {
     execute(oldState){
-        if (oldState.channel.name === 'Hang Out' || oldState.channel.name ===  'Admin Voice') return
+        if (oldState.channel.name === 'Hang Out' || oldState.channel.name ===  'Admin Voice' || oldState.channel.name ===  'Waiting For Squad') return
         if (oldState.channel.members.size < 1) oldState.channel.delete('Channel Empty')
     }
 }
