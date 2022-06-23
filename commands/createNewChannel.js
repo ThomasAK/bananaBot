6c87101fa873851ddc8ca481316edf57995a1850
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-06-23 09:19:49 -0600

    CreateChannel  userRate if set to 0 will be unlimited so added that as the default. as well added the higher bitrate for the channel.

--- commands/createNewChannel.js
+++ commands/createNewChannel.js
@@ -8,7 +8,8 @@ module.exports = {
         if (message.guild.channels.cache.find(channel => channel.name === messageDetails[1])) return message.channel.send('Channel name already taken')
         await message.guild.channels.create(messageDetails[1] , {
             type: 'GUILD_VOICE',
-            userLimit: messageDetails[2] ? messageDetails[2] : 99
+            userLimit: messageDetails[2] ? messageDetails[2] : 0,
+            bitrate: 128
         })
 
         const newChannel =await message.guild.channels.cache.find(channel => channel.name === messageDetails[1])
