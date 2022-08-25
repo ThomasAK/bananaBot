718700f75212e073056060ac5570fbc6b40f8299
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2022-08-24 22:10:54 -0600

    Fix issue with streamingMessage where it will post streaming activity even when member is not currently using that guild.

--- commands/streamingMessage.js
+++ commands/streamingMessage.js
@@ -3,7 +3,7 @@ const data = require("../data.json")
 module.exports = {
     name: 'streamingMessage',
     async execute(presence, activity){
-        if (!presence.member.voice.guild.equals(presence.guild)) return
+        if (presence.member.voice.guild.name !== data.guilds[0].name) return
        const channel = await presence.guild.channels.cache.get(data.guilds[0].streamingChannel)
         channel.send(`@here \n ${presence.member.displayName} is STREAMING ${activity.details} NOW! \n ${activity.url}`)
     }
