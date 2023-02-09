78c37a55e09765d6a2ea395644b76c54fd07cea4
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2023-02-09 00:00:03 -0700

    add roll logging

--- commands/roll.js
+++ commands/roll.js
@@ -3,7 +3,9 @@ module.exports = {
     description: 'Dice roll!',
     execute(message){
         const messageDetails = message.content.split(/ +/)
+        console.log(messageDetails[1])
         const dieNumber = Math.floor(Math.random() * messageDetails[1]);
+        console.log(dieNumber)
         message.channel.send(dieNumber)
     }
 }
