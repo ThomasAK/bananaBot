2c27473a0c920ed23d513cb231287df863cdfedc
Thomas A Kempton <Thomas.a.k.tk@gmail.com>
2023-10-04 09:40:10 -0600

    fix remove channel

--- commands/removeChannel.js
+++ commands/removeChannel.js
@@ -1,8 +1,8 @@
 const guildList = ['Sans Pareil', 'TheBananaBoys']
 class RemoveChannel {
     execute(oldState){
-        if (oldState.channel.name === 'Hang Out' || oldState.channel.name ===  'Admin Voice' || oldState.channel.name ===  'Waiting For Squad' || oldState.channel.name ===  'AFK') return
-        if (oldState.channel.members.size < 1 && guildList.find( guild => guild === oldState.channel.guild.name)) oldState.channel.delete('Channel Empty')
+        if (oldState.channel.name === 'Hang Out' || oldState.channel.name ===  'Admin Voice' || oldState.channel.name ===  'Waiting For Squad' || oldState.channel.name ===  'AFK' || !guildList.find( guild => guild === oldState.channel.guild.name)) return
+        if (oldState.channel.members.size < 1) oldState.channel.delete('Channel Empty')
     }
 }
 
