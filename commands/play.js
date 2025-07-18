798720c2edfee24351df129b129ac177441860d4
thomas A Kempton <thomas.a.k.tk@gmail.com>
2025-07-17 23:28:29 -0600

    update to discord.js 14 remove yt-search add play-dl for searching since it has spotify searching. remove ytdl-core as it is no longer being updated and regularly breaks add youtube-dl-exec as it is a reliable downloader with the only issue is how slow it is.

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,14 @@
-const ytdl = require('ytdl-core')
-const ytSearch = require('yt-search')
-const {joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus} = require('@discordjs/voice')
+const play = require("play-dl") 
+
+const {joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, NoSubscriberBehavior, VoiceConnectionStatus} = require('@discordjs/voice')
 const player = createAudioPlayer()
+const ytdl = require("youtube-dl-exec")
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+const { raw } = require("mysql2")
+const TEMP_DIR = path.join(os.tmpdir(), 'discord-music-bot');
 
 const queue = new Map()
 // queue(message.guild.id, queue_constructor object {voice channel, text channel, connection, song[]})
@@ -43,22 +50,65 @@ const clear_queue = async (guild) => {
     queue.delete(guild.id)
 }
 
+async function downloadAudio(videoUrl) {
+  console.log(`Downloading audio from: ${videoUrl}`);
+  
+  // Generate a unique filename based on timestamp
+  const timestamp = Date.now();
+  const outputFile = path.join(TEMP_DIR, `audio-${timestamp}.mp3`);
+  
+  try {
+    // Use youtube-dl-exec to download audio only
+    await ytdl(videoUrl, {
+      extractAudio: true,
+      audioFormat: 'mp3',
+      audioQuality: 0, // Best quality
+      output: outputFile,
+      noCheckCertificates: true,
+      noWarnings: true,
+      preferFreeFormats: true,
+      addHeader: ['referer:youtube.com', 'user-agent:Mozilla/5.0'],
+    });
+    
+    console.log(`Downloaded audio to: ${outputFile}`);
+    return outputFile;
+  } catch (error) {
+    console.error('Error downloading audio:', error);
+    throw error;
+  }
+}
+
 //Create song resource then play song.
 const video_player = async (message, song) =>{
     const server_queue = queue.get(message.guild.id)
     if (!server_queue) return
     try {
-        const stream = await ytdl(await song.url, {filter: 'audioonly',
-        requestOptions: {
-            headers: {
-                Cookie: 'LOGIN_INFO=AFmmF2swRAIgXI2owcG_TQ2_F-OPwNFLVoh3NAZAYs1D2ku9MQt0350CIBq-zclJS1V49zBBbl3JIJLAYM1M1vizhhmb6BXQe9xJ:QUQ3MjNmemxtRGZGSkYyVklqa09CTVBUYVpaTDF2UFItbXMtN2p1OE05SWJtNktQYUpxU014allqbGpoOUk1RXVZcWhMWjRma3RwLUg0alRKLVBkWHRFVFh5OVRuaWM0MXdrQmJ4a0RCdW85S3J1bFdMeHowMDBKbTJxTEJ3N2Y1V012VEtTelF1OHlNa1FGanB4SjRmQmV2X09heXlveFl3; VISITOR_PRIVACY_METADATA=CgJVUxICGgA%3D; HSID=AApdn_4cLPcwyva3g; SSID=AygT3JeWepxO-HZ4b; APISID=Q4rFMhzNUZ-VuxK7/ASJ7LjN8wtplNeEZ-; SAPISID=T7d-rorgCnARILcT/ARcxCFdvhbFCA92qH; __Secure-1PAPISID=T7d-rorgCnARILcT/ARcxCFdvhbFCA92qH; __Secure-3PAPISID=T7d-rorgCnARILcT/ARcxCFdvhbFCA92qH; VISITOR_INFO1_LIVE=oBrrx3iCyQ8; PREF=f6=40000000&f7=4100&tz=America.Denver&f5=20000; SID=dAhgiyhbMahdTrwEl7TyP5Zh9tg6vXpfmpncyWN4N0d9KTEuzfMgLjR9l06Gir0zEvVYkw.; __Secure-1PSID=dAhgiyhbMahdTrwEl7TyP5Zh9tg6vXpfmpncyWN4N0d9KTEuYq61e8WtP6adZe2LzGCN9A.; __Secure-3PSID=dAhgiyhbMahdTrwEl7TyP5Zh9tg6vXpfmpncyWN4N0d9KTEu7e3iz9H5o9Qzr756PcKPng.; YSC=K_gzaj_5BY4; __Secure-1PSIDTS=sidts-CjIBNiGH7kEmS2VA-utTO2bqEiarW4Cxj2nTui2RVQ_9M7TPO0G8CWKCRQ6EVhDLuYfcwxAA; __Secure-3PSIDTS=sidts-CjIBNiGH7kEmS2VA-utTO2bqEiarW4Cxj2nTui2RVQ_9M7TPO0G8CWKCRQ6EVhDLuYfcwxAA; SIDCC=ACA-OxNH5go0br2vXGrSw5RgKhR1NTDUXhnRLAW0wWS7Fp6YoF_XbRAfTpWrkOO7U6tboOwQL7E; __Secure-1PSIDCC=ACA-OxNLETCpFXCmdZayQ-VTYx_4A9vVVbKiqwFpvdjSlAj40tE1uVEpngm7V9cXYuNapvFgv2U; __Secure-3PSIDCC=ACA-OxMkPXNRoOG2wYS7_QbPFA7Op2EhBOqEnQ60dffYZtGpDFC_jeTWTQLQ_eVjYpWC6x2skOs; ST-10eigm9=itct=CH0QyCAYASITCKuc9qznvYIDFeBmTAgdTX4NsDIDQkZhSPHmtozOiJ2xXJoBBQgMEPgd&csn=MC41Nzk2NTE0MzYxOTY2OQ..&endpoint=%7B%22clickTrackingParams%22%3A%22CH0QyCAYASITCKuc9qznvYIDFeBmTAgdTX4NsDIDQkZhSPHmtozOiJ2xXJoBBQgMEPgd%22%2C%22commandMetadata%22%3A%7B%22webCommandMetadata%22%3A%7B%22url%22%3A%22%2Fwatch%3Fv%3DRm7OLlwtNL0%26list%3DOLAK5uy_lhflSRynwMpe8kPIzMrEWH27EqXg_FBEc%26index%3D2%26pp%3D8AUB%22%2C%22webPageType%22%3A%22WEB_PAGE_TYPE_WATCH%22%2C%22rootVe%22%3A3832%7D%7D%2C%22watchEndpoint%22%3A%7B%22videoId%22%3A%22Rm7OLlwtNL0%22%2C%22playlistId%22%3A%22OLAK5uy_lhflSRynwMpe8kPIzMrEWH27EqXg_FBEc%22%2C%22index%22%3A1%2C%22params%22%3A%22OAE%253D%22%2C%22playerParams%22%3A%228AUB%22%2C%22loggingContext%22%3A%7B%22vssLoggingContext%22%3A%7B%22serializedContextData%22%3A%22GilPTEFLNXV5X2xoZmxTUnlud01wZThrUEl6TXJFV0gyN0VxWGdfRkJFYw%253D%253D%22%7D%7D%2C%22watchEndpointSupportedOnesieConfig%22%3A%7B%22html5PlaybackOnesieConfig%22%3A%7B%22commonConfig%22%3A%7B%22url%22%3A%22https%3A%2F%2Frr1---sn-qpgxjujvhh-naje.googlevideo.com%2Finitplayback%3Fsource%3Dyoutube%26oeis%3D1%26c%3DWEB%26oad%3D3200%26ovd%3D3200%26oaad%3D11000%26oavd%3D11000%26ocs%3D700%26oewis%3D1%26oputc%3D1%26ofpcc%3D1%26beids%3D24350018%26siu%3D1%26msp%3D1%26odepv%3D1%26id%3D466ece2e5c2d34bd%26ip%3D206.251.42.135%26initcwndbps%3D943750%26mt%3D1699769336%26oweuc%3D%26pxtags%3DCg4KAnR4Egg1MTAxODg2NA%26rxtags%3DCg4KAnR4Egg1MTAxODg2NA%252CCg4KAnR4Egg1MTAxODg2NQ%252CCg4KAnR4Egg1MTAxODg2Ng%252CCg4KAnR4Egg1MTAxODg2Nw%252CCg4KAnR4Egg1MTAxODg2OA%252CCg4KAnR4Egg1MTAxODg2OQ%252CCg4KAnR4Egg1MTAxODg3MA%22%7D%7D%7D%7D%7D'
+        
+        const audioFile = await downloadAudio(song.url);
+    
+        // Create a read stream from the downloaded file
+        const audioStream = fs.createReadStream(audioFile);
+        
+        // Create an audio resource from the file stream
+        const resource = createAudioResource(audioStream, {
+        inlineVolume: true
+        });
+
+        player.on('stateChange', (oldState, newState) => {
+            if (newState.status === 'idle') {
+                console.log(`Cleaning up temp file: ${audioFile}`);
+                fs.unlink(audioFile, (err) => {
+                if (err) console.error('Error removing temp file:', err);
+                });
             }
-        }});
-        const resource = await createAudioResource(stream)
+        })
+        
         await player.play(resource)
         await message.channel.send(`Now Playing ${song.title}`)
+        
     }catch (err) {
         console.log(`${err} video_player failure?`)
+        console.trace(err);
         message.channel.send('Song failed to play.')
         player.stop()
         if (server_queue.songs.length === 0) {
@@ -73,19 +123,19 @@ const video_player = async (message, song) =>{
 //If args[0] is  url then get info through url and pass it back.
 //If args[0] is not url locate song in YouTube then collect URL and then pass song info back
 const getSongURL = async (message, args)=>{
-    if (ytdl.validateURL(args[0])) {
-        const song_info = await ytdl.getInfo(args[0]);
-        return  {title: song_info.videoDetails.title, url: song_info.videoDetails.video_url}
+    if (play.validate(args[0]) && await play.validate(args[0]) != "search") {
+        console.log(await play.validate(args[0]))
+        const song_info = await play.video_info(args[0]);
+        return  {title: song_info.video_details.title, url: song_info.video_details.url}
     } else {
         //If the video is not a URL then use keywords to find that video.
-        const video_finder = async (query) => {
-            const videoResults = await ytSearch(query);
-            return (videoResults.videos.length > 1) ? videoResults.videos[0] : null
-        }
-
-        const video = await video_finder(args.join(' '));
+        
+        const video = await play.search(args.toString(), {
+            limit: 1
+        })
+    
         if (video) {
-            return  {title: video.title, url: video.url}
+            return  {title: video[0].title, url: video[0].url}
         } else {
             message.channel.send('Unable to find video');
         }
@@ -111,8 +161,10 @@ const setUpServerQueue = async (message, voice_channel, song)=>{
             guildId: voice_channel.guild.id,
             adapterCreator: voice_channel.guild.voiceAdapterCreator,
         })
-        await server_queue.connection.subscribe(player)
+        
+        
         await video_player(message, await server_queue.songs.shift())
+        await server_queue.connection.subscribe(player)
         await sleep(1000)
         await player.on('error', async error => {
             console.error(`${error} audio player Error....... \n ${server_queue.songs}`);
@@ -133,6 +185,7 @@ const setUpServerQueue = async (message, voice_channel, song)=>{
             }
 
         });
+        
         await player.on(AudioPlayerStatus.Idle, async () => {
             console.log(server_queue.songs)
             if (server_queue.songs.length === 0) {
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const { Client, Intents, Collection, MessageEmbed} = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS], partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'PRESENCE_UPDATE', 'GUILD_MEMBER','GUILDS'] });
-const activityRole =  require('./commands/activityRole')
-//const runActivity = new activityRole()
+const { Client, GatewayIntentBits, Collection, MessageEmbed, Partials} = require('discord.js');
+//const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS], partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'PRESENCE_UPDATE', 'GUILD_MEMBER','GUILDS'] });
+const intents = [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildVoiceStates, 
+    GatewayIntentBits.GuildPresences, 
+    GatewayIntentBits.GuildMembers
+]
+const client = new Client({intents: intents,
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.GuildMember, Partials.Guilds]
+});
+//const activityRole =  require('./commands/activityRole')
 const data = require("./data.json")
 const removeChannel = require("./commands/removeChannel")
 
 const prefix = '!!';
 
 const fs = require('fs');
+const { log } = require('console');
 
 client.commands = new Collection();
 
@@ -21,32 +32,33 @@ client.on('ready', () => {
     console.log('Mr Bot is online!')
 })
 
-client.on('messageCreate',  message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
+client.on('messageCreate',  async (message) => {
+    const msg = await message.fetch()
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+    const args = msg.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     switch (command){
         case 'ping':
-            client.commands.get("ping").execute(message);
+            client.commands.get("ping").execute(msg);
             break;
 
         case 'reactionrole':
-            client.commands.get('reactionrolesetup').execute(message);
+            client.commands.get('reactionrolesetup').execute(msg);
             break;
         case 'timeout':
             const user = args.shift()
-            client.commands.get('timeout').execute(message, user.slice(3, user.length - 1))
+            client.commands.get('timeout').execute(msg, user.slice(3, user.length - 1))
             break;
         case 'lf':
-            client.commands.get('lf').execute(message).then()
+            client.commands.get('lf').execute(msg).then()
             break;
-        case 'setinactive':
-            client.commands.get('setInactive').execute(message).then()
-            break;
+        // case 'setinactive':
+        //     client.commands.get('setInactive').execute(message).then()
+        //     break;
         case 'createchannel':
         case 'cc':
-            client.commands.get('createChannel').execute(message)
+            client.commands.get('createChannel').execute(msg)
             break;
         case 'play':
         case 'skip':
@@ -55,23 +67,23 @@ client.on('messageCreate',  message => {
         case 'resume':
         case 'clear':
         case 'queue':
-            client.commands.get('play').execute(message, args, command)
+            client.commands.get('play').execute(msg, args, command)
             break;
         case 'roll':
-            client.commands.get('roll').execute(message)
+            client.commands.get('roll').execute(msg)
             break;
         case 'guilds':
             const Guilds = client.guilds.cache.map(guild => guild.id);
-            message.channel.send(`Servers: ${Guilds.join(',')}`)
+            msg.channel.send(`Servers: ${Guilds.join(',')}`)
             break;
         case 'commands':
-            message.channel.send('!!play !!skip !!pause !!resume !!clear !!queue !!leave \n !!roll')
+            msg.channel.send('!!play !!skip !!pause !!resume !!clear !!queue !!leave \n !!roll')
             break;
         default:
             const embed = new MessageEmbed()
-                .setDescription(`!!${command} is not a command please try again ${message.member.displayName}! \n To see a list of commands use !!commands.`)
+                .setDescription(`!!${command} is not a command please try again ${msg.member.displayName}! \n To see a list of commands use !!commands.`)
                 .setImage("https://media.giphy.com/media/z1bE45A1GsyT6/giphy.gif");
-            message.channel.send({embeds: [embed]})
+            msg.channel.send({embeds: [embed]})
             break;
     }
 })
@@ -80,7 +92,7 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     //if (oldState.channel == null && newState != null) await runActivity.updateStatus(newState)
     if (!oldState.channelId) return
     const guildList = ['Sans Pareil', 'TheBananaBoys']
-    if (oldState.channel !== newState.channel && guildList.find( guild => guild === oldState.channel.guild.name)) await removeChannel.execute(oldState)
+    //if (oldState.channel !== newState.channel && guildList.find( guild => guild === oldState.channel.guild.name)) await removeChannel.execute(oldState)
 })
 
 client.on('messageReactionAdd', async (reaction, user) => {
